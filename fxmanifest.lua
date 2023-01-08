2d13abff5320c6929f2ada7e15534514f3205216
Randolio <103154494+Randolio@users.noreply.github.com>
2023-01-08 09:21:31 +0000

    Make config.lua a shared script and fix spelling typo.

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,14 +2,15 @@ fx_version 'cerulean'
 game 'gta5'
 
 author 'Glowie'
-description 'Tresure hunting script using digiscanner'
+description 'Treasure hunting script using digiscanner'
 version '1.0'
+
+shared_script 'config.lua'
 
 client_script 'client.lua'
 
 server_scripts {
 	'server.lua',
-	'config.lua'
 }
 
 lua54 'yes'
